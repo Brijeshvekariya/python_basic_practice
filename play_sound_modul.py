899c21d4f46cae540e48c3b9ecf7fb9c2179308c
Brijeshvekariya <bvekariya54@gmail.com>
2023-09-15 18:15:37 +0530

    updated

--- play_sound_modul.py
+++ play_sound_modul.py
@@ -1,3 +1,4 @@
 from playsound import playsound
 
-playsound('D:/Projects/FL/finacta/src/assets/audio/notification-sound.mp3')
+playsound('D:\\Projects\\FL\\finacta\\src\\assets\\audio\\notification-sound.mp3')
+print("playing sound")
