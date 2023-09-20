136c2c06a8dd1956729e7f62403fea321d6efb9e
Brijeshvekariya <bvekariya54@gmail.com>
2023-09-20 10:59:18 +0530

    updated

---
+++ raise_custom_error.py
@@ -0,0 +1,18 @@
+'''
+in python we can raise custom errors byusing keyword 'raise'
+
+
+'''
+
+salary = int(input("Enter your salary : "))
+if not 2000 < salary < 5000:
+    print("salary is not in range")
+    raise ValueError("value")
+
+try:
+    age = int(input("Enter your age : "))
+    if age<18:
+        raise ValueError("You cannot get darru")
+
+except ValueError as var:
+    print(var)
