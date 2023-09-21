2ed16c81455e2d9140d741af9a48d8db88d72a0b
Brijeshvekariya <bvekariya54@gmail.com>
2023-09-21 18:54:08 +0530

    updated

--- while.py
+++ while.py
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
+
 i = 1 #initilization
 while i<=5:
     print(i) #statement
-=======
-i = 1 #initilization
-while i<=5:
-    print(i) #statement
->>>>>>> 3b71dde1f7ecb0185ed1d65d1357b3003d5e95a2
+
     i+=1 #updation
