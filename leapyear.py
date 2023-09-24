9b897fa2892e24cee7c7b75e0f40acace3177354
Brijeshvekariya <bvekariya54@gmail.com>
2023-09-24 16:17:42 +0530

    updated

--- leapyear.py
+++ leapyear.py
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
+
 year = int(input("Enter Year : "))
 if year%4==0:
     print("The Given Year is Leap Year")
 else:
-=======
-year = int(input("Enter Year : "))
-if year%4==0:
-    print("The Given Year is Leap Year")
-else:
->>>>>>> 3b71dde1f7ecb0185ed1d65d1357b3003d5e95a2
     print("The Given Year is Not Leap Year")
