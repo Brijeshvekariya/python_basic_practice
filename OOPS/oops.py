1b1fd682899faa4c838ceef87a3472b0522293e1
Brijeshvekariya <bvekariya54@gmail.com>
2023-09-25 18:34:27 +0530

    updated

---
+++ OOPS/oops.py
@@ -0,0 +1,39 @@
+'''
+oops :- Object Oriented Programming System or Structure
+
+class :- Class is an user defined datatype and it isa collection of objects. It is a logical entity that has some specific attributes and methods.
+
+or
+
+class :- class is collection of data member and member function.
+
+datamember :- variable
+member function :- methods
+
+Object :- Object is an instance or a variable of class
+
+Everything in world is object
+
+c=male,female - object
+class - HUman
+
+interview Question :-
+
+difference between function and method:-
+
+Functon :- Function is declared outside of class
+methods :- Methods are declared within the class
+
+OOPS main topic :
+
+1. class
+2. object
+3. Constructor
+4. Encapsulation
+5. Inheritance
+6. Abstraction
+7. Polymorphism
+
+self :- This is used to define that method is of current class
+
+'''
