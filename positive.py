214011599ce50e479932e358eccfa50cacc9a1d1
Brijeshvekariya <bvekariya54@gmail.com>
2023-09-26 16:39:15 +0530

    updated

--- positive.py
+++ positive.py
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
+
 num = float(input("Enter Any Number : "))
 if num>=0:
     print("The Given Number is Positive")
 else:
     print("The Given Number is Negative")
-=======
-num = float(input("Enter Any Number : "))
-if num>=0:
-    print("The Given Number is Positive")
-else:
-    print("The Given Number is Negative")
->>>>>>> 3b71dde1f7ecb0185ed1d65d1357b3003d5e95a2
+
