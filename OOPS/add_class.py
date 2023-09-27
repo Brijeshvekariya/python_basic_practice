adfe7e098f5f4e27f581df5cefef5f42366d1b19
Brijeshvekariya <bvekariya54@gmail.com>
2023-09-27 18:19:10 +0530

    updated

---
+++ OOPS/add_class.py
@@ -0,0 +1,7 @@
+class Addition:
+    def __init__(self,a,b):
+        print(f"The addition of {a} and {b} is : ",a+b)
+
+a= int(input("Enter first Number : "))
+b = int(input("Enter Second Number : "))
+obj = Addition(a,b)
