d2acc23ee816b8f396451d83b88ab32fa40d7645
Brijeshvekariya <bvekariya54@gmail.com>
2023-09-29 18:42:37 +0530

    updated

--- OOPS/Inheritance.py
+++ OOPS/Inheritance.py
@@ -31,11 +31,23 @@ Types of Inheritance:-
             |
         Class C
 
+4. Hierarchical Inheritance :- when one parent class and have multiple child class
 
+        class A
+            |
+    class B   class C
+
+5. Hybrid Inheritance :- it is combination of different inheritance
+
+                 class A
+                    |
+            class B   Class C
+                    |
+                class D
 '''
 
 # Single - Level Inheritance
-class Parent:
+'''class Parent:
     def house(self):
         print("3 BHK House")
 
@@ -45,7 +57,7 @@ class Child(Parent):
 
 c = Child()
 c.car() 
-c.house()
+c.house()'''
 
 # Multi-level Inheritance
 '''class Grandfather:
@@ -66,7 +78,7 @@ c.house()
 c.car()'''
 
 # Multiple Inheritance
-class Father:
+'''class Father:
     def house(self):
         print("5 BHk house")
 
@@ -80,4 +92,49 @@ class Son (Father,Uncle):
 c = Son()
 c.land()
 c.house()
-c.car()
+c.car()'''
+
+# Hierarchical Inheritance
+class Parent:
+    def parent(self):
+        print("This is parent class")
+
+class Child1(Parent):
+    def child1(self):
+        print("This is child1")
+
+class Child2(Parent):
+    def child2(self):
+        print("This is child2")
+
+#object creation
+c1 = Child1()
+c2 = Child2()
+
+c1.parent()
+c1.child1()
+c2.parent()
+c2.child2()
+
+
+'''class Grandfather:
+    def land(self):
+        print("20 acres of land")
+
+class Father(Grandfather):
+    def shops(self):
+        print("5 shops")
+
+class Uncle(Grandfather):
+    def car(self):
+        print("BMW car")
+
+class Son(Father,Uncle):
+    def factory(self):
+        print("Iron casting Factory")
+
+s1 = Son()
+s1.land()
+s1.shops()
+s1.car()
+s1.factory()'''
